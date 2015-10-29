9ea9871da9c7b570894e063e70636f00dcba68ab
fongchun <fongchunchan@gmail.com>
2015-10-29 16:55:44 +0000

    Adding Package Docs

---
+++ R/titanCNAutils.R
@@ -0,0 +1,7 @@
+#' titanCNAutils: An R package for working TitanCNA Results
+#'
+#' titanCNAutils provides various utility functions for post-processing
+#' TitanCNA results.
+#' @docType package
+#' @name titanCNAutils
+NULL
